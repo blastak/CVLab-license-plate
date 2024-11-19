c2c80a5956712700e7175dc03c7d600781056146
Ray Kim <blastak@naver.com>
2024-11-19 16:38:59 +0900

    IoU 기반 MOT 구현

--- LP_Tracking/MultiObjectTracker.py
+++ LP_Tracking/MultiObjectTracker.py
@@ -1,21 +1,32 @@
 import cv2
+import numpy as np
+from scipy.optimize import linear_sum_assignment
 
 from LP_Detection.VIN_LPD import load_model_VinLPD
 from LinearKalmanFilter import LinearKalmanFilter
-
-import numpy as np
-from scipy.optimize import linear_sum_assignment
 from Utils import iou
+
+colors = [(0, 0, 255),
+          (0, 128, 255),
+          (0, 255, 255),
+          (0, 255, 0),
+          (255, 0, 0),
+          (128, 0, 0),
+          (255, 0, 255)
+          ]
+
 
 def xywh2xyxy(xywh):
     assert len(xywh) == 4
     xyxy = [xywh[0], xywh[1], xywh[0] + xywh[2], xywh[1] + xywh[3]]
     return xyxy
 
+
 def xyxy2xywh(xyxy):
     assert len(xyxy) == 4
     xywh = [xyxy[0], xyxy[1], xyxy[2] - xyxy[0], xyxy[3] - xyxy[1]]
     return xywh
+
 
 def xywh2cxcywh(xywh):
     assert len(xywh) == 4
@@ -25,6 +36,7 @@ def xywh2cxcywh(xywh):
     cxcywh = [cx, cy, w, h]
     return cxcywh
 
+
 def cxcywh2xywh(cxcywh):
     assert len(cxcywh) == 4
     w, h = cxcywh[2], cxcywh[3]
@@ -32,6 +44,7 @@ def cxcywh2xywh(cxcywh):
     y = cxcywh[1] - h / 2
     xywh = [x, y, w, h]
     return xywh
+
 
 def cxcywh2cxcysfar(cxcywh):
     assert len(cxcywh) == 4
@@ -41,6 +54,7 @@ def cxcywh2cxcysfar(cxcywh):
     cxcysfar = [cxcywh[0], cxcywh[1], sf, ar]
     return cxcysfar
 
+
 def cxcysfar2cxcywh(cxcysfar):
     assert len(cxcysfar) == 4
     sf, ar = cxcysfar[2], cxcysfar[3]
@@ -48,6 +62,7 @@ def cxcysfar2cxcywh(cxcysfar):
     h = sf / w
     cxcywh = [cxcysfar[0], cxcysfar[1], w, h]
     return cxcywh
+
 
 class Track:
     __id: int = 0
@@ -58,7 +73,7 @@ class Track:
         A = np.eye(dim_x, dtype=np.float32)
         A[:3, 4:] = np.eye(3)
         H = np.eye(dim_z, dim_x, dtype=np.float32)
-        Q = np.eye(dim_x, dtype=np.float32) * 1
+        Q = np.eye(dim_x, dtype=np.float32) * 0.01
         R = np.eye(dim_z, dtype=np.float32) * 1
         x0 = np.float32([cx, cy, sf, ar, 0, 0, 0])
         self.kf = LinearKalmanFilter(dim_x, A, H, Q, R, x0)
@@ -69,240 +84,137 @@ class Track:
         self.cnt_total_vis = 0
         self.cnt_consecutive_invis = 0
 
-        self.bounding_box = x0[:4].tolist()
-        self.trajectory = [self.bounding_box]
+        self.last_xyxy = xywh2xyxy(cxcywh2xywh(cxcysfar2cxcywh(x0[:4])))
+        self.trajectory = []
+        self.color = colors[Track.__id % len(colors)]
 
     def predict(self):
         x_ = self.kf.predict()
-        cx, cy = x_[:2]
-        return cx, cy
+        # x_[2] = max(0, x_[2])  # sf 가 음수가 되는 경우를 방지
+        self.last_xyxy = xywh2xyxy(cxcywh2xywh(cxcysfar2cxcywh(x_[:4])))
+        print(self.last_xyxy)
+        return self.last_xyxy
 
     def correct(self, z):
         x = self.kf.correct(z)
+        self.last_xyxy = xywh2xyxy(cxcywh2xywh(cxcysfar2cxcywh(x[:4])))
+        return self.last_xyxy
+
+    def update_trajectory(self):
+        self.trajectory.append(self.last_xyxy)
+        return self.trajectory
 
 
 class Tracker:
     def __init__(self):
         self.tracks: list[Track] = []
+        self.detections = []
+        self.predictions = []
 
     def track(self, detections):
-        predictions = self.predict_new_location_of_tracks()
-        assignments, unassignedTracks, unassignedDetections = self.assign_detection_to_track(predictions, detections)
-        self.update_assigned_tracks()
-        self.update_unassigned_tracks()
+        self.detections = detections
+        self.predict_new_location_of_tracks()
+        assigned_td_pairs, unassigned_trks, unassigned_dets = self.assign_detection_to_track()
+        self.update_assigned_tracks(assigned_td_pairs)
+        self.update_unassigned_tracks(unassigned_trks)
         self.delete_lost_tracks()
-        self.create_new_tracks()
+        self.create_new_tracks(unassigned_dets)
 
     def predict_new_location_of_tracks(self):
-        predictions = []
+        self.predictions.clear()
         for tr in self.tracks:
-            cx, cy = tr.predict()
-            predictions.append([cx, cy])
-        return predictions
+            xyxy = tr.predict()
+            self.predictions.append(xyxy)
 
-    def assign_detection_to_track(self, predictions, detections):
-        self.detections = detections
-        if len(predictions) == 0:
-            return np.empty((0, 2), dtype=int), np.arange(len(detections)), np.empty((0, 5), dtype=int)
+    def assign_detection_to_track(self):
+        N = len(self.predictions)
+        M = len(self.detections)
 
-        iou_matrix = np.zeros((len(detections), len(predictions)), dtype=np.float32)
-        for d, det in enumerate(detections):
-            for t, trk in enumerate(predictions):
-                iou_matrix[d, t] = iou(det, trk)
+        assigned_td_pairs = []
+        unassigned_trks = list(range(N))
+        unassigned_dets = list(range(M))
 
-        cost_matrix = 1 - iou_matrix
-        matched_indices = linear_sum_assignment(cost_matrix)
-        matched_indices = np.array(list(zip(*matched_indices)))
-        unmatched_detections = []
-        for d, det in enumerate(detections):
-            if d not in matched_indices[:, 0]:
-                unmatched_detections.append(d)
-        unmatched_trackers = []
-        for t, trk in enumerate(predictions):
-            if t not in matched_indices[:, 1]:
-                unmatched_trackers.append(t)
+        if N * M != 0:
+            mat_iou = np.zeros((N, M), dtype=np.float32)
+            for i, trk in enumerate(self.predictions):
+                for j, det in enumerate(self.detections):
+                    mat_iou[i, j] = iou(trk, det)
+            mat_cost = 1 - mat_iou
+            matched = linear_sum_assignment(mat_cost)
 
-        matches = []
-        for m in matched_indices:
-            if iou_matrix[m[0], m[1]] < iou_threshold:
-                unmatched_detections.append(m[0])
-                unmatched_trackers.append(m[1])
-            else:
-                matches.append(m.reshape(1, 2))
-        if len(matches) == 0:
-            matches = np.empty((0, 2), dtype=int)
-        else:
-            matches = np.concatenate(matches, axis=0)
-        return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+            for t, d in list(zip(*matched)):
+                THRESH_IOU = 0.1
+                if mat_iou[t, d] >= THRESH_IOU:
+                    assigned_td_pairs.append([t, d])
+                    unassigned_trks.remove(t)
+                    unassigned_dets.remove(d)
+
+        return assigned_td_pairs, unassigned_trks, unassigned_dets
 
     def update_assigned_tracks(self, td_pairs):
-        for t,d in td_pairs:
-            cx,cy,sf,ar = cxcywh2cxcysfar(xywh2cxcywh(xyxy2xywh(self.detections[d])))
+        for t, d in td_pairs:
+            cx, cy, sf, ar = cxcywh2cxcysfar(xywh2cxcywh(xyxy2xywh(self.detections[d])))
             z = np.float32([cx, cy, sf, ar])
             self.tracks[t].correct(z)
             self.tracks[t].age += 1
             self.tracks[t].cnt_consecutive_invis = 0
 
-    def update_unassigned_tracks(self, t_lists):
-        for t in t_lists:
+    def update_unassigned_tracks(self, t_idxs):
+        for t in t_idxs:
             self.tracks[t].age += 1
             self.tracks[t].cnt_consecutive_invis += 1
 
     def delete_lost_tracks(self):
-        for i,trk in enumerate(self.tracks[:]):  # colon ":" 안 붙이면 del과 충돌
+        for trk in self.tracks:
             THRESH_CONSECUTIVE_INVISIBLE = 10
-            if trk.cnt_consecutive_invis >THRESH_CONSECUTIVE_INVISIBLE:
-                del self.tracks[i]
+            if trk.cnt_consecutive_invis > THRESH_CONSECUTIVE_INVISIBLE:
+                self.tracks.remove(trk)
 
-    def create_new_tracks(self, d_lists):
-        for d in d_lists:
+    def create_new_tracks(self, d_idxs):
+        for d in d_idxs:
             xyxy = self.detections[d]
-
-
+            cxcysfar = cxcywh2cxcysfar(xywh2cxcywh(xyxy2xywh(xyxy)))
+            new_track = Track(*cxcysfar)
+            self.tracks.append(new_track)
 
 
 if __name__ == '__main__':
     d_net = load_model_VinLPD('../LP_Detection/VIN_LPD/weight')  # VIN_LPD 사용 준비
     cap = cv2.VideoCapture('./sample_video/sample1.avi')
-    # cap = cv2.VideoCapture(r"D:\백업_비젼인_테스트동영상\20181129_테크윈카메라_인하대입구_2미터\009_20181129_085645_30대.avi")
-    tracks = []
 
+    myTracker = Tracker()
+
+    cnt_continue = 0
     while cap.isOpened():
         is_grabbed = cap.grab()
         if not is_grabbed:
+            cnt_continue += 1
+            if cnt_continue > 3:
+                break
             continue
         succ, img_orig = cap.retrieve()
         if not succ:
             break
+        cnt_continue = 0
 
         # detection
         d_out = d_net.resize_N_forward(img_orig)
 
-        detections = []
+        xyxys = []
         for _, d in enumerate(d_out):
-            detections.append((d.x, d.y, d.w, d.h))
-        # detections = detect_objects(frame)
-        predictions = predict_new_locations_of_tracks(tracks)
-        matches, unmatched_detections, unmatched_tracks = detection_to_track_assignment(detections, predictions)
+            xyxys.append(xywh2xyxy([d.x, d.y, d.w, d.h]))
 
-        update_assigned_tracks(tracks, detections, matches)
-        update_unassigned_tracks(tracks, unmatched_tracks)
-        tracks = delete_lost_tracks(tracks)
-        create_new_tracks(tracks, detections, unmatched_detections)
+        myTracker.track(xyxys)
 
-        display_tracking_results(frame, tracks)
+        for _, trk in enumerate(myTracker.tracks):
+            trajectories = trk.update_trajectory()
+            for traj in trajectories:
+                traj = list(map(int, traj))
+                cv2.rectangle(img_orig, traj[:2], traj[2:], color=trk.color, thickness=2)
 
+        cv2.imshow('img_orig', img_orig)
         if cv2.waitKey(1) == 27:
             break
 
     cap.release()
     cv2.destroyAllWindows()
-
-# # Track 객체 정의
-# class Track:
-#     def __init__(self, cx, cy):
-#         # LinearKalmanFilter 초기화
-#         A = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=np.float32)
-#         H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], dtype=np.float32)
-#         Q = np.eye(4, dtype=np.float32) * 0.03
-#         R = np.eye(2, dtype=np.float32) * 1
-#         x0 = np.array([bbox[0], bbox[1], 0, 0], dtype=np.float32)
-#         self.kf = LinearKalmanFilter(dim_x=4, A=A, H=H, Q=Q, R=R, x0=x0)
-#         self.bbox = list(bbox)
-#         self.age = 1
-#         self.consecutive_invisible_count = 0
-#         self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
-#
-#     def predict(self):
-#         pred = self.kf.predict()
-#         return (pred[0], pred[1])
-#
-#     def correct(self, measurement):
-#         x_hat = self.kf.correct(np.array([measurement[0], measurement[1]], dtype=np.float32))
-#         self.bbox = [*x_hat[:2], 550 // 4, 110 // 4]
-#         self.age += 1
-#         self.consecutive_invisible_count = 0
-#
-#
-# # 객체 탐지 함수
-# def detect_objects(frame):
-#     # YCbCr 색 공간으로 변환
-#     ycbcr_image = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
-#
-#     # 녹청색 중심값 (166, 70, 130)을 기준으로 범위 설정
-#     y_center, cb_center, cr_center = 166, 70, 130
-#     y_range, cb_range, cr_range = 50, 20, 20  # Y, Cb, Cr 범위 설정
-#
-#     lower_cyan = np.array([y_center - y_range, cb_center - cb_range, cr_center - cr_range], dtype=np.uint8)
-#     upper_cyan = np.array([y_center + y_range, cb_center + cb_range, cr_center + cr_range], dtype=np.uint8)
-#
-#     # 녹청색 픽셀 마스크 생성
-#     cyan_mask = cv2.inRange(ycbcr_image, lower_cyan, upper_cyan)
-#     cyan_mask = cv2.dilate(cyan_mask, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)))
-#
-#     # 마스크로부터 윤곽선 탐지
-#     contours, _ = cv2.findContours(cyan_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-#     # 윤곽선마다 bounding box 그리기
-#     bboxes = [cv2.boundingRect(contour) for contour in contours]
-#
-#     return bboxes
-#
-#
-# # 새 위치 예측 함수
-# def predict_new_locations_of_tracks(tracks):
-#     predictions = [track.predict() for track in tracks]
-#     return predictions
-#
-#
-# # 탐지 결과와 트랙의 매칭 함수
-# def detection_to_track_assignment(detections, predictions):
-#     cost_matrix = np.zeros((len(detections), len(predictions)))
-#     for i, det in enumerate(detections):
-#         for j, pred in enumerate(predictions):
-#             cost_matrix[i][j] = np.linalg.norm(np.array(det[:2]) - np.array(pred))
-#     row_ind, col_ind = linear_sum_assignment(cost_matrix)
-#     matches, unmatched_detections, unmatched_tracks = [], [], []
-#     for i, det in enumerate(detections):
-#         if i in row_ind:
-#             matches.append((i, col_ind[np.where(row_ind == i)[0][0]]))
-#         else:
-#             unmatched_detections.append(i)
-#     for j, pred in enumerate(predictions):
-#         if j not in col_ind:
-#             unmatched_tracks.append(j)
-#     return matches, unmatched_detections, unmatched_tracks
-#
-#
-# # 매칭된 트랙 업데이트
-# def update_assigned_tracks(tracks, detections, matches):
-#     for det_idx, track_idx in matches:
-#         tracks[track_idx].correct(detections[det_idx][:2])
-#
-#
-# # 매칭되지 않은 트랙 업데이트
-# def update_unassigned_tracks(tracks, unmatched_tracks):
-#     for idx in unmatched_tracks:
-#         tracks[idx].consecutive_invisible_count += 1
-#
-#
-# # 오래된 트랙 삭제
-# def delete_lost_tracks(tracks):
-#     return [track for track in tracks if track.consecutive_invisible_count < 5]
-#
-#
-# # 새로운 트랙 생성
-# def create_new_tracks(tracks, detections, unmatched_detections):
-#     for i in unmatched_detections:
-#         new_track = Track(detections[i])
-#         tracks.append(new_track)
-#
-#
-# # 결과 표시
-# def display_tracking_results(frame, tracks):
-#     for track in tracks:
-#         x, y, w, h = track.bbox
-#         cv2.rectangle(frame, (int(x), int(y)), (int(x + w), int(y + h)), track.color, 2)
-#     cv2.imshow('Tracking', frame)
-#
-#
