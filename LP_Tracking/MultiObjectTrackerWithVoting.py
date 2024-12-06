89e8fdac477c8367a6ccd09fe1bc636a543fed0c
Ray Kim <blastak@naver.com>
2024-12-06 15:09:35 +0900

    7자리 이상의 레이블을 찾은 경우에만 correction

--- LP_Tracking/MultiObjectTrackerWithVoting.py
+++ LP_Tracking/MultiObjectTrackerWithVoting.py
@@ -1,35 +1,82 @@
-from LP_Recognition.VIN_OCR import load_model_VinOCR
-from MultiObjectTracker import *
-from Utils import trans_eng2kor_v1p3
+import cv2
+import numpy as np
 
-class TrackWithLabel(Track):
-    def __init__(self, cx, cy, sf, ar, label):
+from LP_Detection.VIN_LPD import load_model_VinLPD
+from LP_Recognition.VIN_OCR import load_model_VinOCR
+from MultiObjectTracker import Track, Tracker
+from Utils import trans_eng2kor_v1p3
+from Utils import xywh2xyxy, cxcywh2xywh, cxcysfar2cxcywh, cxcywh2cxcysfar, xywh2cxcywh, xyxy2xywh
+from collections import Counter
+
+class TrackWithPlateNumber(Track):
+    def __init__(self, cx, cy, sf, ar, p_type, p_number):
         super().__init__(cx, cy, sf, ar)
-        self.labels = []
-        self.plate_types = []
+        self.plate_types = Counter({p_type: 1})
+        self.plate_numbers = [p_number]
+        self.voted_type = ''
+        self.voted_number = ''
+
+    def correct2(self, z, p_type, p_number):
+        x = self.kf.correct(z)
+        self.last_xyxy = xywh2xyxy(cxcywh2xywh(cxcysfar2cxcywh(x[:4])))
+
+        self.plate_types[p_type] += 1
+        self.plate_numbers.append(p_number)
+        self.vote()
+
+        return self.last_xyxy
+
+    def vote(self):
+        print(self.plate_types.most_common())
+        print(self.plate_types.most_common()[0][0])
+        pass
 
 
 class TrackerWithVoting(Tracker):
     def __init__(self):
         super().__init__()
-        self.tracks: list[TrackWithLabel] = []
+        self.tracks: list[TrackWithPlateNumber] = []
+
+    def track2(self, detections, p_types, p_numbers):
+        self.detections = detections
+        self.predict_new_location_of_tracks()
+        assigned_td_pairs, unassigned_trks, unassigned_dets = self.assign_detection_to_track()
+        self.update_assigned_tracks2(assigned_td_pairs, p_types, p_numbers)
+        self.update_unassigned_tracks(unassigned_trks)
+        self.delete_lost_tracks()
+        self.create_new_tracks2(unassigned_dets, p_types, p_numbers)
+
+    def update_assigned_tracks2(self, td_pairs, p_types, p_numbers):
+        for t, d in td_pairs:
+            cx, cy, sf, ar = cxcywh2cxcysfar(xywh2cxcywh(xyxy2xywh(self.detections[d])))
+            z = np.float32([cx, cy, sf, ar])
+            self.tracks[t].correct2(z, p_types[d], p_numbers[d])
+            self.tracks[t].age += 1
+            self.tracks[t].cnt_consecutive_invis = 0
+
+    def create_new_tracks2(self, d_idxs, p_types, p_numbers):
+        for d in d_idxs:
+            xyxy = self.detections[d]
+            cxcysfar = cxcywh2cxcysfar(xywh2cxcywh(xyxy2xywh(xyxy)))
+            new_track = TrackWithPlateNumber(*cxcysfar, p_types[d], p_numbers[d])
+            self.tracks.append(new_track)
 
 
 if __name__ == '__main__':
     d_net = load_model_VinLPD('../LP_Detection/VIN_LPD/weight')  # VIN_LPD 사용 준비
     r_net = load_model_VinOCR('../LP_Recognition/VIN_OCR/weight')
     # cap = cv2.VideoCapture('./sample_video/sample1.avi')
-    cap = cv2.VideoCapture(r"D:\Dataset\01_LicensePlate\06_Techwin_B1_in_\20180523_171757_합본(in).avi")  # 테크윈B1 - in
+    cap = cv2.VideoCapture(r"D:\Dataset\01_LicensePlate\08_Inha_Entrance_\20181129_084819_합본.avi")
 
     myTracker = TrackerWithVoting()
 
     cnt_continue = 0
-    cnt_frame = 0
+    cnt_frame = 5700
     delay = 0
+    cap.set(cv2.CAP_PROP_POS_FRAMES, cnt_frame)
     while cap.isOpened():
         is_grabbed = cap.grab()
         cnt_frame += 1
-        if cnt_frame < 500: continue
         if not is_grabbed:
             cnt_continue += 1
             if cnt_continue > 3:
@@ -46,20 +93,25 @@ if __name__ == '__main__':
         d_out = d_net.resize_N_forward(img_orig)
 
         xyxys = []
+        types = []
+        numbers = []
         for _, d in enumerate(d_out):
-            # recognition
-            img_crop = r_net.crop_resize_with_padding(img_orig, d)
-            r_out = r_net.resize_N_forward(img_crop)
-            list_char = r_net.check_align(r_out, d.class_idx + 1)
-            list_char_kr = trans_eng2kor_v1p3(list_char)
-            label = ''.join(list_char_kr)
-            print(label)
-
-            xyxys.append(xywh2xyxy([d.x, d.y, d.w, d.h]))
             cv2.rectangle(img_disp, (d.x, d.y, d.w, d.h), color=(255, 255, 255), thickness=1)
             cv2.circle(img_disp, (d.x + d.w // 2, d.y + d.h // 2), 1, (0, 255, 0), 3)
 
-        myTracker.track(xyxys)
+            # recognition
+            img_crop = r_net.crop_resize_with_padding(img_orig, d)
+            r_out = r_net.resize_N_forward(img_crop)
+            list_char, _ = r_net.check_align(r_out, d.class_idx + 1)
+            list_char_kr = trans_eng2kor_v1p3(list_char)
+            label = ''.join(list_char_kr)
+
+            if len(label) >= 7:
+                types.append(d.class_str)
+                numbers.append(label)
+                xyxys.append(xywh2xyxy([d.x, d.y, d.w, d.h]))
+
+        myTracker.track2(xyxys, types, numbers)
 
         for _, trk in enumerate(myTracker.tracks):
             trajectories = trk.update_trajectory()
