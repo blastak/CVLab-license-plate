9a36a910ed3293d8f3b96d6811b2a2256ccb940d
MINCHAN <amc8574@naver.com>
2024-12-06 13:29:23 +0900

    중심점 비교 Hungarian Algorithm으로 변경

--- Data_Labeling/compare_OCRBB.py
+++ Data_Labeling/compare_OCRBB.py
@@ -3,7 +3,7 @@ import os
 
 import cv2
 import numpy as np
-from tensorflow.python.lib.io.file_io import rename
+from scipy.optimize import linear_sum_assignment
 from tqdm import tqdm
 
 from Graphical_Model_Generation.Graphical_Model_Generator_KOR import Graphical_Model_Generator_KOR
@@ -30,7 +30,7 @@ if __name__ == '__main__':
         img_paths = []
         folder_path = os.path.join(prefix_path, folder_name)
         if os.path.isdir(folder_path) and folder_name.startswith('front_'):
-            img_paths.extend(os.path.join(folder_path, file_name) for file_name in os.listdir(folder_path) if file_name.endswith('.jpg'))
+            img_paths.extend(file_name for file_name in os.listdir(folder_path) if file_name.endswith('.jpg'))
 
         r_net = load_model_VinOCR('../LP_Recognition/VIN_OCR/weight')
 
@@ -38,10 +38,8 @@ if __name__ == '__main__':
 
         plate_type = folder_path.split('_')[-1]
         char_xywh = generator.LP_char_xywh.get(plate_type)
-        reference_point = (int(char_xywh[0][0]), int(char_xywh[0][1]), int(char_xywh[1][0]), int(char_xywh[1][1]))
-        reference_point = calculate_center(reference_point)
         for _, img_path in tqdm(enumerate(img_paths), total=len(img_paths), desc=f'{folder_name}'):
-            img = imread_uni(img_path)  # 이미지 로드
+            img = imread_uni(os.path.join(folder_path, img_path))  # 이미지 로드
             plate_number = img_path.split('_')[-1][:-4]
 
             height, width = img.shape[:2]
@@ -58,49 +56,53 @@ if __name__ == '__main__':
             if len(r_out) < 3:
                 continue
             list_char, box = r_net.check_align(r_out, int(folder_name[-1]))
-            # for i, b in enumerate(box):
-            #     if plate_type == 'P3' or plate_type == 'P4' or plate_type == 'P5':
-            #         if i == 0:
-            #             cv2.rectangle(padded_img, (b.x, b.y, b.w, b.h), (255, 255, 0), 1)  # bounding box
-            #             cv2.rectangle(padded_img, (int(char_xywh[i * 2][0]), int(char_xywh[i * 2][1]), int(char_xywh[i * 2 + 1][0]), int(char_xywh[i * 2 + 1][1])), (255, 255, 255), 1)
-            #         elif i > 1:
-            #             cv2.rectangle(padded_img, (b.x, b.y, b.w, b.h), (255, 255, 0), 1)  # bounding box
-            #             cv2.rectangle(padded_img, (int(char_xywh[(i-1) * 2][0]), int(char_xywh[(i-1) * 2][1]), int(char_xywh[(i-1) * 2 + 1][0]), int(char_xywh[(i-1) * 2 + 1][1])), (255, 255, 255), 1)
-            #     else:
-            #         cv2.rectangle(padded_img, (b.x, b.y, b.w, b.h), (255, 255, 0), 1)  # bounding box
-            #         cv2.rectangle(padded_img, (int(char_xywh[i * 2][0]), int(char_xywh[i * 2][1]), int(char_xywh[i * 2 + 1][0]), int(char_xywh[i * 2 + 1][1])), (255, 255, 255), 1)
-
             list_char_kr = trans_eng2kor_v1p3(list_char)
             print(''.join(list_char_kr))
 
-            # cv2.imshow("Padded Image", padded_img)
-            # cv2.waitKey()
+            # Bounding Box visualize
+            for i, b in enumerate(box):
+                cv2.rectangle(padded_img, (b.x, b.y, b.w, b.h), (255, 255, 0), 1)  # bounding box
+            for i in range(len(char_xywh) // 2):
+                cv2.rectangle(padded_img, (int(char_xywh[i * 2][0]), int(char_xywh[i * 2][1]), int(char_xywh[i * 2 + 1][0]), int(char_xywh[i * 2 + 1][1])), (255, 255, 255), 1)
 
-            threshold = 25
+            threshold = 20
 
-            if len(list_char_kr) == len(plate_number):
-                offset_sum = 0
-                for i, b in enumerate(box):
-                    if plate_type == 'P3' or plate_type == 'P4' or plate_type == 'P5':
-                        if i==0:
-                            center1 = calculate_center([b.x, b.y, b.w, b.h])
-                            center2 = calculate_center([box[1].x, box[1].y, box[1].w, box[1].h])
-                            mid = (center1 + center2) // 2
-                            reference_point = calculate_center([int(char_xywh[i * 2][0]), int(char_xywh[i * 2][1]), int(char_xywh[i * 2 + 1][0]), int(char_xywh[i * 2 + 1][1])])
-                            offset_sum += np.linalg.norm(mid - reference_point)
-                        elif i > 1:
-                            center = calculate_center([b.x, b.y, b.w, b.h])
-                            reference_point = calculate_center([int(char_xywh[(i-1) * 2][0]), int(char_xywh[(i-1) * 2][1]), int(char_xywh[(i-1) * 2 + 1][0]), int(char_xywh[(i-1) * 2 + 1][1])])
-                            offset_sum += np.linalg.norm(center - reference_point)
-                    else:
-                        center = calculate_center([b.x, b.y, b.w, b.h])
-                        reference_point = calculate_center([int(char_xywh[i * 2][0]), int(char_xywh[i * 2][1]), int(char_xywh[i * 2 + 1][0]), int(char_xywh[i * 2 + 1][1])])
-                        offset_sum += np.linalg.norm(center - reference_point)
-                print(offset_sum)
-                if offset_sum <= threshold:  # threshold보다 가까우면 저장
-                    filtered_files.append(plate_number)
-                    save_path = os.path.join(prefix_path, 'good_' + plate_type)
-                    if not os.path.exists(save_path):
-                        os.makedirs(save_path)
-                    rename(img_path, os.path.join(save_path, img_path.split('\\')[-1]))
-                print("추출 파일 목록:", len(filtered_files))
+            # 숫자 기준점
+            reference_points = []
+            if plate_type == 'P3' or plate_type == 'P4' or plate_type == 'P5':
+                num = [1, 2, 4, 5, 6, 7]
+            elif plate_type == 'P1-3' or plate_type == 'P1-4':
+                num = [0, 1, 2, 4, 5, 6, 7]
+            else:  # P1-1, P1-2, P2, P6
+                num = [0, 1, 3, 4, 5, 6]
+            for i in num:
+                ref_center = calculate_center([int(char_xywh[i * 2][0]), int(char_xywh[i * 2][1]), int(char_xywh[i * 2 + 1][0]), int(char_xywh[i * 2 + 1][1])])
+                reference_points.append(ref_center)
+
+            # bounding_box 중심점
+            bbox_centers = []
+            for i, b in enumerate(box):
+                center = calculate_center([b.x, b.y, b.w, b.h])
+                bbox_centers.append(center)
+
+            cost_matrix = np.zeros((len(reference_points), len(bbox_centers)))
+            for i, ref_point in enumerate(reference_points):
+                for j, bbox_point in enumerate(bbox_centers):
+                    cost_matrix[i, j] = np.linalg.norm(ref_point - bbox_point)
+            row_ind, col_ind = linear_sum_assignment(cost_matrix)
+            print(row_ind, col_ind)
+            offset_sum = 0
+            for r, c in zip(row_ind, col_ind):
+                print(f"Reference Point {r} is matched with Bounding Box {c}")
+                print(f"Distance: {cost_matrix[r, c]:.2f}")
+                offset_sum += cost_matrix[r, c]
+            print(offset_sum)
+            if offset_sum <= threshold:
+                print('correct')
+                save_path = os.path.join(prefix_path, f'good_{plate_type}_Hungarian{threshold}')
+                if not os.path.exists(save_path):
+                    os.makedirs(save_path)
+                os.rename(os.path.join(folder_path, img_path), os.path.join(save_path, img_path))
+
+            cv2.imshow("Padded Image", padded_img)
+            cv2.waitKey(0)
