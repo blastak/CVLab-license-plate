564623f06cee0d2914d918cc40af4574f39d293d
Ray Kim <blastak@naver.com>
2024-12-12 16:01:37 +0900

    calc_relative_angle 함수화

--- Data_Labeling/calculate_relative_angle.py
+++ Data_Labeling/calculate_relative_angle.py
@@ -8,6 +8,72 @@ from Data_Labeling.Dataset_Loader.DatasetLoader_WebCrawl import DatasetLoader_We
 from Data_Labeling.Graphical_Model_Generation.Graphical_Model_Generator_KOR import Graphical_Model_Generator_KOR as GMG
 from Utils import imread_uni, add_text_with_background
 
+
+def calc_relative_angle(xy1, xy2, xy3, xy4, plate_type, image_width, image_height):
+    """
+    이미지 내 번호판의 상대적인 각도 계산
+
+    이 함수는 이미지 내 번호판의 네 꼭짓점 좌표, 번호판 종류, 이미지 크기를 입력받아
+    3차원 공간에서 번호판의 회전 각도를 추정합니다.
+
+    Args:
+        xy1 (tuple): 번호판 왼쪽 상단 꼭짓점 좌표
+        xy2 (tuple): 번호판 오른쪽 상단 꼭짓점 좌표
+        xy3 (tuple): 번호판 오른쪽 하단 꼭짓점 좌표
+        xy4 (tuple): 번호판 왼쪽 하단 꼭짓점 좌표
+        plate_type (str): 번호판 종류
+        image_width (int): 이미지 너비
+        image_height (int): 이미지 높이
+
+    Returns:
+        list: 번호판의 상대적인 회전 각도 (x, y, z)를 요소로 하는 리스트 (단위: 도)
+    """
+
+    if plate_type not in GMG.LP_plate_wh.keys():
+        raise NotImplementedError
+
+    # 3D 상의 점
+    vw, vh = GMG.LP_plate_wh[plate_type]
+    vmax = max(vh, vw)
+    vh /= vmax
+    vw /= vmax
+    canonical_rect = [
+        [[-vw / 2], [-vh / 2], [0]], [[vw / 2], [-vh / 2], [0]],
+        [[vw / 2], [vh / 2], [0]], [[-vw / 2], [vh / 2], [0]],
+    ]
+
+    # virtual camera matrix
+    focal_length = max(image_width, image_height)
+    camera_matrix = np.float64([[focal_length, 0, image_width / 2],
+                                [0, focal_length, image_height / 2],
+                                [0, 0, 1]])
+    distortion_matrix = np.zeros((4, 1), dtype=np.float64)
+
+    # quad-box centering
+    projected_points_f = np.float32([xy1, xy2, xy3, xy4])
+    centering_offset = projected_points_f.mean(axis=0) - np.array([image_width, image_height]) / 2
+    projected_points_f -= centering_offset  # centering을 하지 않으면 translation 값 때문에 rotation의 해석이 어렵게 되어버린다.
+
+    # solvePnP
+    pts3d = np.float64(canonical_rect).squeeze(2)
+    pts2d = np.float64(projected_points_f)
+    success, rot_vec, trans_vec = cv2.solvePnP(pts3d, pts2d, camera_matrix, distortion_matrix, flags=cv2.SOLVEPNP_ITERATIVE)
+
+    # # reprojection test
+    # reproj_, jacobian = cv2.projectPoints(pts3d, rot_vec, trans_vec, camera_matrix, distortion_matrix)
+    # reproj = np.int32(reproj_.squeeze(1) + centering_offset)
+
+    angles_in_deg = [0, 0, 0]
+    if success:
+        rmat, jac = cv2.Rodrigues(rot_vec)  # Get rotational matrix
+        angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)  # Get angles
+        if math.isnan(any(angles)):
+            angles_in_deg = [0, 0, 0]
+        else:
+            angles_in_deg = [-angles[0], -angles[1], angles[2]]
+    return angles_in_deg
+
+
 if __name__ == '__main__':
     prefix = './Dataset_Loader/sample_image_label/PM'
     loader = DatasetLoader_WebCrawl(prefix)
@@ -17,56 +83,13 @@ if __name__ == '__main__':
         img_orig = imread_uni(os.path.join(prefix, jpg_path))
         plate_type, plate_number, xy1, xy2, xy3, xy4, left, top, right, bottom = loader.parse_json(loader.list_json[l])
 
-        if plate_type not in GMG.LP_plate_wh.keys():
-            raise NotImplementedError
-
-        # 3D 상의 점
-        vw, vh = GMG.LP_plate_wh[plate_type]
-        vmax = max(vh, vw)
-        vh /= vmax
-        vw /= vmax
-        canonical_rect = [
-            [[-vw / 2], [-vh / 2], [0]], [[vw / 2], [-vh / 2], [0]],
-            [[vw / 2], [vh / 2], [0]], [[-vw / 2], [vh / 2], [0]],
-        ]
-
-        # virtual camera matrix
         iw, ih = img_orig.shape[1::-1]
-        focal_length = max(iw, ih)
-        camera_matrix = np.float64([[focal_length, 0, iw / 2],
-                                    [0, focal_length, ih / 2],
-                                    [0, 0, 1]])
-        distortion_matrix = np.zeros((4, 1), dtype=np.float64)
-
-        # quad-box centering
-        projected_points_f = np.float32([xy1, xy2, xy3, xy4])
-        centering_offset = projected_points_f.mean(axis=0) - np.array([iw, ih]) / 2
-        projected_points_f -= centering_offset
-
-        # solvePnP
-        pts3d = np.float64(canonical_rect).squeeze(2)
-        pts2d = np.float64(projected_points_f)
-        success, rot_vec, trans_vec = cv2.solvePnP(pts3d, pts2d, camera_matrix, distortion_matrix, flags=cv2.SOLVEPNP_ITERATIVE)
-
-        if success:
-            rmat, jac = cv2.Rodrigues(rot_vec)  # Get rotational matrix
-            angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)  # Get angles
-            if math.isnan(any(angles)):
-                print('nan')
-            else:
-                angles_x = -angles[0]
-                angles_y = -angles[1]
-                angles_z = angles[2]
-                print('x:%.2f y:%.2f z:%.2f' % (angles_x, angles_y, angles_z))
-
-        # projection result
-        reproj_, jacobian = cv2.projectPoints(pts3d, rot_vec, trans_vec, camera_matrix, distortion_matrix)
-        reproj = np.int32(reproj_.squeeze(1) + centering_offset)
+        angle_xyz = calc_relative_angle(xy1, xy2, xy3, xy4, plate_type, iw, ih)
+        print('x:%-10.2f y:%-10.2f z:%-10.2f' % tuple(angle_xyz))
 
         img_disp = img_orig.copy()
         cv2.rectangle(img_disp, (left, top), (right, bottom), (255, 255, 0), 3)  # bounding box
         cv2.polylines(img_disp, [np.int32([xy1, xy2, xy3, xy4])], True, color=(255, 0, 255), thickness=2, lineType=cv2.LINE_AA)  # quadrilateral box
-        # cv2.polylines(img_disp, [reproj], True, color=(255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
         font_size = (right - left) // 5  # magic number
         img_disp = add_text_with_background(img_disp, plate_number, position=(left, top - font_size), font_size=font_size, padding=0)
 
