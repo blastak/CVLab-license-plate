46d02e5eb71792b36bae8829ec86b711bd48be2a
Ray Kim <blastak@naver.com>
2025-01-23 23:56:56 +0900

    최초 web app 데모

--- Demo/web_demo.py
+++ Demo/web_demo.py
@@ -4,8 +4,13 @@ import uuid
 import cv2
 import gradio as gr
 
+from Utils import get_pretty_datetime
+
+from inferencing import Demo_Runner
+
 SUBSAMPLE = 2
 
+demo_runner = Demo_Runner()
 
 def stream_object_detection(video, password):
     vc = cv2.VideoCapture(video)
@@ -27,14 +32,16 @@ def stream_object_detection(video, password):
     chunk_name = os.path.join(output_path, f"output_{uuid.uuid4()}.mp4")
     vw = cv2.VideoWriter(chunk_name, fourcc, desired_fps, (w_2, h_2))
 
+    demo_runner.setup()
+
     while retval:
         if n_frames % SUBSAMPLE == 0:
             img = cv2.resize(img, (w_2, h_2))
             batch.append(img)
         if len(batch) == 2 * desired_fps:
             for b in batch:
-
-                vw.write(b)
+                img2, img3 = demo_runner.loop(b, password, password)
+                vw.write(img2)
 
             vw.release()
             yield chunk_name
@@ -46,33 +53,30 @@ def stream_object_detection(video, password):
         retval, img = vc.read()
         n_frames += 1
 
-    print('finish')
+    print(get_pretty_datetime(), password)
 
 
 with gr.Blocks() as demo:
     gr.HTML(
         """
     <h1 style='text-align: center'>
-    License plate swapping
+    License Plate De-identification (Encryption)
     </h1>
     """
     )
     with gr.Row():
         with gr.Column():
             video = gr.Video(label="Video Source")
+            box = gr.Textbox(label='password for encryption', value='1q2w3e')
         with gr.Column():
             output_video = gr.Video(
-                label="Processed Video", streaming=True, autoplay=True, loop=True
-            )
-            box = gr.Textbox(
-                label='password for encryption',
-                value='1q2w3e'
+                label="Encrypted Video", streaming=True, autoplay=True, loop=True
             )
 
     video.upload(
         fn=stream_object_detection,
         inputs=[video, box],
-        outputs=[output_video],
+        outputs=[output_video]
     )
 
 if __name__ == "__main__":
